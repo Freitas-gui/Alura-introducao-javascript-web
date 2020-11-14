0f5bed529699251ab9d36ac37820b7e788a3ba47
Guilheme Freitas <freitas-gui19@hotmail.com>
2020-11-14 18:55:43 -0300

    add new row

--- introducao-javascript/js/main.js
+++ introducao-javascript/js/main.js
@@ -21,3 +21,40 @@ for(var i=0; i<pacientes.length; i++){
     else
         imc.textContent = calcImc.toFixed(2)
 }
+
+
+var buttomAdd = document.querySelector("#adicionar-paciente")
+
+buttomAdd.addEventListener("click", function(event){
+    event.preventDefault()
+    
+    var form = document.querySelector("#form-add")
+
+    var nome = form.nome.value
+    var peso = form.peso.value
+    var altura = form.altura.value
+    var gordura = form.gordura.value
+
+    var pacienteTr = document.createElement("tr")
+
+    var nomeTd = document.createElement("td")
+    var pesoTd = document.createElement("td")
+    var alturaTd = document.createElement("td")
+    var gordurTtd = document.createElement("td")
+    var imcTd = document.createElement("td")
+
+    nomeTd.textContent = nome
+    pesoTd.textContent = peso
+    alturaTd.textContent = altura
+    gordurTtd.textContent = gordura
+
+    pacienteTr.appendChild(nomeTd)
+    pacienteTr.appendChild(pesoTd)
+    pacienteTr.appendChild(alturaTd)
+    pacienteTr.appendChild(gordurTtd)
+
+    var tabelaPacientes = document.querySelector("#tabela-pacientes")
+    tabelaPacientes.appendChild(pacienteTr)
+
+
+})
